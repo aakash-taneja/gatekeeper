cf54ba13122e1afcc3709f1d806654b4fd363758
aakash-taneja <19uec168@lnmiit.ac.in>
2021-06-11 21:14:11 +0530

    axios

--- src/components/accountBox/loginForm.jsx
+++ src/components/accountBox/loginForm.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext,useState } from "react";
 import {
   BoldLink,
   BoxContainer,
@@ -9,15 +9,40 @@ import {
 } from "./common";
 import { Marginer } from "../marginer";
 import { AccountContext } from "./accountContext";
+import Axios from'axios';
 
 export function LoginForm(props) {
   const { switchToSignup } = useContext(AccountContext);
 
+
+  const url="/api";
+  const [data,setdata]=useState({
+    email:"",
+    pass:""    
+  })
+
+  function handle(e){
+    const newdata={...data}
+    newdata[e.target.id]=e.target.value
+    setdata(newdata)
+    console.log(newdata);
+  }
+  function submit(e){
+    e.preventDefault();
+    Axios.post(url,{
+      email:data.email,
+      pass:data.pass
+    })
+    .then(res=>{
+        console.log(res.data)
+    })
+  }
+
   return (
     <BoxContainer>
-      <FormContainer>
-        <Input type="email" placeholder="Email" />
-        <Input type="password" placeholder="Password" />
+      <FormContainer onSubmit={(e)=>submit(e)}>
+        <Input type="email" placeholder="Email" onChange={(e)=>handle(e)} id="email" value={data.email} />
+        <Input type="password" placeholder="Password" onChange={(e)=>handle(e)} id="pass" value={data.pass} />
       </FormContainer>
       <Marginer direction="vertical" margin={10} />
       <MutedLink href="#">Forget your password?</MutedLink>
--- src/components/accountBox/signupForm.jsx
+++ src/components/accountBox/signupForm.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext,useState } from "react";
 import {
   BoldLink,
   BoxContainer,
@@ -9,17 +9,44 @@ import {
 } from "./common";
 import { Marginer } from "../marginer";
 import { AccountContext } from "./accountContext";
+import Axios from'axios';
 
 export function SignupForm(props) {
   const { switchToSignin } = useContext(AccountContext);
 
+  const url="/api1";
+  const [data,setdata]=useState({
+    name:"",
+    email:"",
+    password:"" ,
+    confPassword:""
+  })
+
+  function handle(e){
+    const newdata={...data}
+    newdata[e.target.id]=e.target.value
+    setdata(newdata)
+    console.log(newdata);
+  }
+  function submit(e){
+    e.preventDefault();
+    Axios.post(url,{
+      name:data.name,
+      email:data.email,
+      password:data.password,
+      confPassword:data.confPassword
+    })
+    .then(res=>{
+        console.log(res.data)
+    })
+  }
   return (
     <BoxContainer>
-      <FormContainer>
-        <Input type="text" placeholder="Full Name" />
-        <Input type="email" placeholder="Email" />
-        <Input type="password" placeholder="Password" />
-        <Input type="password" placeholder="Confirm Password" />
+      <FormContainer onSubmit={(e)=>submit(e)}>
+        <Input type="text" placeholder="Full Name" onChange={(e)=>handle(e)} id="fullName" value={data.fullName}/>
+        <Input type="email" placeholder="Email" onChange={(e)=>handle(e)} id="email" value={data.email}/>
+        <Input type="password" placeholder="Password" onChange={(e)=>handle(e)} id="password" value={data.password}/>
+        <Input type="password" placeholder="Confirm Password" onChange={(e)=>handle(e)} id="confPassword" value={data.confPassword}/>
       </FormContainer>
       <Marginer direction="vertical" margin={10} />
       <SubmitButton type="submit">Signup</SubmitButton>
@@ -30,6 +57,7 @@ export function SignupForm(props) {
           Signin
         </BoldLink>
       </MutedLink>
+      <Marginer direction="vertical" margin={10} />
     </BoxContainer>
   );
 }
